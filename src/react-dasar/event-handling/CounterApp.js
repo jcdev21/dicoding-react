837793a6de4f04d2681a833b53666f0b4a620f79
jcdev21 <jcdev21@gmail.com>
2022-06-14 11:40:11 +0700

    class component & event handling

---
+++ src/react-dasar/event-handling/CounterApp.js
@@ -0,0 +1,78 @@
+import * as React from 'react';
+
+function CounterDisplay({ count }) {
+	if (count === 0) {
+		return <p>{count}</p>;
+	}
+
+	if (count % 5 === 0 && count % 7 === 0) {
+		return <p>FizzBuzz</p>;
+	}
+
+	if (count % 5 === 0) {
+		return <p>Fizz</p>;
+	}
+
+	if (count % 7 === 0) {
+		return <p>Buzz</p>;
+	}
+
+	return <p>{count}</p>;
+}
+
+function IncreaseButton({ increase }) {
+	return (
+		<div>
+			<button onClick={increase}>+ increase</button>
+		</div>
+	);
+}
+
+function ResetButton({ reset }) {
+	return (
+		<div>
+			<button onClick={reset}>- reset</button>
+		</div>
+	);
+}
+
+class CounterApp extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: 0,
+		};
+
+		this.onIncreaseEventHandler = this.onIncreaseEventHandler.bind();
+		this.onResetEventHandler = this.onResetEventHandler.bind();
+	}
+
+	onIncreaseEventHandler() {
+		this.setState((preventState) => {
+			return {
+				...preventState,
+				count: preventState.count + 1,
+			};
+		});
+	}
+
+	onResetEventHandler() {
+		this.setState(() => {
+			return {
+				count: 0,
+			};
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<IncreaseButton increase={this.onIncreaseEventHandler} />
+				<CounterDisplay count={this.state.count} />
+				<ResetButton reset={this.onResetEventHandler} />
+			</div>
+		);
+	}
+}
+
+export default CounterApp;
